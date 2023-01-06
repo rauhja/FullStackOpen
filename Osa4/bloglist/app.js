cd956c13d9e7e75a5b77bc918cf373af90c61a67
rauhja <82282638+rauhja@users.noreply.github.com>
2023-01-07 00:31:42 +0200

    Added router for 5.17.-5.22. testing

--- Osa4/bloglist/app.js
+++ Osa4/bloglist/app.js
@@ -10,7 +10,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info('connecting to MongoDB')
 
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
@@ -31,8 +31,14 @@ app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/login', loginRouter)
 
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-
 
 module.exports = app
