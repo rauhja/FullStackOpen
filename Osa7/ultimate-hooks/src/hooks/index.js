d95c152c7bd57a9789728cdd6d842ad7cdccfc76
rauhja <82282638+rauhja@users.noreply.github.com>
2023-01-10 13:55:32 +0200

    Exercises 7.4.-7.8. done

---
+++ Osa7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+  
+    const onChange = (event) => {
+      setValue(event.target.value)
+    }
+  
+    return {
+      type,
+      value,
+      onChange
+    }
+  }
+  
+export const useResource = (baseUrl) => {
+    const [resources, setResources] = useState([])
+  
+    useEffect(() => {
+        axios
+        .get(baseUrl)
+        .then(response => {
+            setResources(response.data)
+        })
+    }, [setResources, baseUrl])
+  
+    const create = async (resource) => {
+        const response = await axios.post(baseUrl, resource)
+        setResources(resources.concat(response.data))
+    }
+  
+    const service = {
+      create
+    }
+  
+    return [
+      resources, service
+    ]
+}
