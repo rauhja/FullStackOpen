3ea3ec8ce6f8c953bc63824ba8044ebb59f24ffb
rauhja <82282638+rauhja@users.noreply.github.com>
2024-08-27 18:28:02 +0300

    exercise 8.9 done

--- Osa8/library-frontend/src/components/Books.jsx
+++ Osa8/library-frontend/src/components/Books.jsx
@@ -1,9 +1,16 @@
+import { useQuery } from "@apollo/client";
+import { ALL_BOOKS } from "../queries";
 const Books = (props) => {
+  const results = useQuery(ALL_BOOKS);
   if (!props.show) {
-    return null
+    return null;
   }
 
-  const books = []
+  if (results.loading) {
+    return <div>Loading...</div>;
+  }
+
+  const books = results.data.allBooks;
 
   return (
     <div>
@@ -26,7 +33,7 @@ const Books = (props) => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
