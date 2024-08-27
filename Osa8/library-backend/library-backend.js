7740cea8ee2c2995cd2e38c2c7af214dc3b7f8be
rauhja <82282638+rauhja@users.noreply.github.com>
2024-08-27 16:57:39 +0300

    exercise 8.7 done

--- Osa8/library-backend/library-backend.js
+++ Osa8/library-backend/library-backend.js
@@ -132,6 +132,11 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `;
 
@@ -162,6 +167,17 @@ const resolvers = {
       const book = { ...args, id: uuid() };
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) {
+        return null;
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };
