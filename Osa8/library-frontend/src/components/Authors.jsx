ba23b1734a6bd092f353051b3a403e66cce6559b
rauhja <82282638+rauhja@users.noreply.github.com>
2024-08-27 18:23:56 +0300

    exercise 8.8 done

--- Osa8/library-frontend/src/components/Authors.jsx
+++ Osa8/library-frontend/src/components/Authors.jsx
@@ -1,8 +1,16 @@
+import { useQuery } from "@apollo/client";
+import { ALL_AUTHORS } from "../queries";
+
 const Authors = (props) => {
+  const results = useQuery(ALL_AUTHORS);
   if (!props.show) {
-    return null
+    return null;
   }
-  const authors = []
+
+  if (results.loading) {
+    return <div>Loading...</div>;
+  }
+  const authors = results.data.allAuthors;
 
   return (
     <div>
@@ -24,7 +32,7 @@ const Authors = (props) => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
