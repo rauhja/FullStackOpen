d173683a6b47c4acb5fde58adeccca88c53a8ace
rauhja <82282638+rauhja@users.noreply.github.com>
2024-09-03 14:11:35 +0300

    exercise 8.20 done

--- Osa8/library-frontend/src/App.jsx
+++ Osa8/library-frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
-import { useApolloClient } from "@apollo/client"; // Add this line to import the useApolloClient hook
+import { useApolloClient, useQuery } from "@apollo/client";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import Login from "./components/Login";
+import Recommend from "./components/Recommend";
+
+import { ME } from "./queries";
 
 const App = () => {
   const [page, setPage] = useState("authors");
   const [token, setToken] = useState(null);
   const client = useApolloClient();
+
+  const result = useQuery(ME);
+  const user = result.data.me;
 
   const logout = () => {
     setToken(null);
@@ -25,6 +31,7 @@ const App = () => {
         {token ? (
           <>
             <button onClick={() => setPage("add")}>add book</button>
+            <button onClick={() => setPage("recommend")}>recommend</button>
             <button onClick={logout}>logout</button>
           </>
         ) : (
@@ -37,6 +44,8 @@ const App = () => {
       <Books show={page === "books"} />
 
       <NewBook show={page === "add"} />
+
+      <Recommend show={page === "recommend"} user={user} />
 
       <Login setToken={setToken} show={page === "login"} />
     </div>
---
+++ Osa8/library-frontend/src/components/Recommend.jsx
@@ -0,0 +1,50 @@
+import { useQuery } from "@apollo/client";
+import { ALL_BOOKS } from "../queries";
+
+const Recommend = ({ show, user }) => {
+  const { data, error, loading } = useQuery(ALL_BOOKS, {
+    variables: { genre: user.favoriteGenre },
+    skip: !show,
+  });
+
+  if (!show) {
+    return null;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  const books = data.allBooks;
+
+  return (
+    <div>
+      <h2>books</h2>
+      <p>
+        books in your favorite genre <b>{user.favoriteGenre}</b>
+      </p>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author.name}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Recommend;
