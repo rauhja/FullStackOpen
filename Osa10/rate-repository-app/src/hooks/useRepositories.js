0e9d5a574ce7d60bc6d28d505d83283ea65d7196
rauhja <82282638+rauhja@users.noreply.github.com>
2025-03-08 13:25:40 +0200

    fix: Exercise 10.11

--- Osa10/rate-repository-app/src/hooks/useRepositories.js
+++ Osa10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
 // const useRepositories = () => {
@@ -27,7 +27,9 @@ const useRepositories = () => {
   const { data, loading, error } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: "cache-and-network",
   });
-  const repositories = data ? data.repositories : [];
+
+  const repositories = data?.repositories || null;
+
   return { repositories, loading, error };
 };
 
