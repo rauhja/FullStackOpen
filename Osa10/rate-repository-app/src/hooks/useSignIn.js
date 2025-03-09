e34cd20060fce88134e2363606a39eb9a26c4a9e
rauhja <82282638+rauhja@users.noreply.github.com>
2025-03-09 14:00:16 +0200

    feat: Add authContext provider

--- Osa10/rate-repository-app/src/hooks/useSignIn.js
+++ Osa10/rate-repository-app/src/hooks/useSignIn.js
@@ -1,7 +1,11 @@
 import { useMutation } from "@apollo/client";
 import { AUTH_USER } from "../graphql/mutations";
+import { useContext } from "react";
+import AuthStorageContext from "../contexts/AuthStorageContext";
+import useAuthStorage from "./useAuthStorage";
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(AUTH_USER);
 
   const signIn = async ({ username, password }) => {
